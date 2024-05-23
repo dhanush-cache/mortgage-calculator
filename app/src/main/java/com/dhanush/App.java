b1a8a0fefa0300149886522d1dcefa46a53b1a24
Anonymous <anonymous@example.com>
2024-05-24 01:31:01 +0530

    A simple mortgage calculator.
    - A simple console application to calculate mortgage.

--- app/src/main/java/com/dhanush/App.java
+++ app/src/main/java/com/dhanush/App.java
@@ -1,7 +1,35 @@
 package com.dhanush;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principal;
+        float monthlyInterestRate;
+        int numberOfPayments;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+
+            System.out.print("Annual Interest Rate: ");
+            float annualInterestRate = scanner.nextFloat();
+            monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+
+            System.out.print("Period(Years): ");
+            byte years = scanner.nextByte();
+            numberOfPayments = years * MONTHS_IN_YEAR;
+        }
+
+        double mortgage = principal * monthlyInterestRate
+                * Math.pow(1 + monthlyInterestRate, numberOfPayments)
+                / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }

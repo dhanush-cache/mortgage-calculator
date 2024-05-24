f17eab0dc3e5cd4d9b341c6623658ca1a84b327c
Anonymous <anonymous@example.com>
2024-05-24 19:45:28 +0530

    Overload the readLine() method.
    - User is not forced to input a valid range.

--- app/src/main/java/com/dhanush/Console.java
+++ app/src/main/java/com/dhanush/Console.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Console {
     private static Scanner scanner = new Scanner(System.in);
 
+    public static double readNumber(String prompt) {
+        System.out.print(prompt + ": ");
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, int min, int max) {
         double value;
         while (true) {

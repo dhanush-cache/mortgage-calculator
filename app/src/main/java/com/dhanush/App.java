688a31458eda1e4ecef246833ce7e21915ff7688
Anonymous <anonymous@example.com>
2024-05-27 16:52:12 +0530

    Extract console() method.

--- app/src/main/java/com/dhanush/App.java
+++ app/src/main/java/com/dhanush/App.java
@@ -6,6 +6,10 @@ public class App {
     static NumberFormat currency = NumberFormat.getCurrencyInstance();
 
     public static void main(String[] args) {
+        console();
+    }
+
+    private static void console() {
         int principal = (int) Console.readNumber("Principal", 1000, 1_000_000);
         float annualInterestRate = (float) Console.readNumber("Annual Interest Rate", 1, 30);
         byte years = (byte) Console.readNumber("Period(Years)", 1, 30);
